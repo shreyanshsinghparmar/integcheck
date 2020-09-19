8d3ee377eab347eb34729afc2b8465969f556341
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-09-19 19:54:36 +0530

    add hash generator functions

--- integcheck.py
+++ integcheck.py
@@ -9,11 +9,36 @@ def usage():
 	-h or --help: Display this message
 	''')
 
-def md5():
+def md5(file, hash):
+	output = hashlib.md5()
+	with open(file, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b"")
+			output.update(chunk)
+	return output.digest()
 
-def sha256():
+
+def sha256(file, hash):
+	output = hashlib.sha256()
+	with open(file, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b"")
+			output.update(chunk)
+	return output.digest()
+
 
 if __name__ == "__main__":
 	if sys.argv[1] == "-h" or sys.argv[1] == "--help":
 		usage()
 	else:
+		print('''List of available hash algorithms:
+			MD5
+			SHA256
+		Enter your choice:
+		''', end=' ')
+		algo = input()
+		algo = algo.upper()
+		file = sys.argv[1]
+		hash = sys.argv[2]
+		if algo == "MD5":
+			md5(file, hash)
+		else if algo == "SHA256":
+			sha256(file, hash)
